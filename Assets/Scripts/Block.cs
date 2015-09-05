79324af98ab7c79a4e893e55cd0ba4d9c53d6dfe
Felipe Augusto <felipowsky@gmail.com>
2015-09-05 20:35:38 -0300

    implemented creation of new blocks after break

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,18 +3,66 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
 
+	public float speedAnimation = 10f;
+
 	public Vector2 matrixIndex;
 	public int colorIndex;
 	public Color color;
+	public Vector2 scaleSize;
+
+	private bool animating = false;
+
+	private SpriteRenderer spriteRenderer;
+
+	void Awake () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
 
 	void Start () {
+		if (!animating) {
+			transform.localScale = scaleSize;
+		}
 	}
 
 	void Update () {
+		if (animating) {
+			Vector2 currentScale = transform.localScale;
+
+			Vector2 newScale = new Vector2(Mathf.Lerp(currentScale.x, scaleSize.x, Time.deltaTime * speedAnimation),
+			                               Mathf.Lerp(currentScale.y, scaleSize.y, Time.deltaTime * speedAnimation));
+
+			transform.localScale = newScale;
+
+			Color newColor = spriteRenderer.color;
+			newColor.a = Mathf.Lerp(newColor.a, 1.0f, Time.deltaTime * speedAnimation);
+
+			spriteRenderer.color = newColor;
+		}
 	}
 
 	void OnMouseDown () {
-		GameManager.instance.BreakBlocks(matrixIndex);
+		GameManager manager = GameManager.instance;
+
+		if (!manager.canInteract) {
+			return;
+		}
+
+		manager.BreakBlocks(matrixIndex);
+	}
+
+	public void ShowAnimated () {
+		if (animating) {
+			return;
+		}
+
+		transform.localScale = Vector2.zero;
+
+		Color newColor = spriteRenderer.color;
+		newColor.a = 0.0f;
+
+		spriteRenderer.color = newColor;
+
+		animating = true;
 	}
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,14 @@ public class GameManager : MonoBehaviour {
 	public static GameManager instance;
 
 	public Transform blockPrefab;
-	public int rows = 5;
-	public int columns = 5;
+	public Transform startPointMatrix;
 
 	public Vector2 scaleSize = new Vector2(1, 1);
 
-	public Transform startPointMatrix;
+	public int rows = 5;
+	public int columns = 5;
+
+	public bool canInteract = true;
 
 	private Transform[,] matrix;
 	private Color[] colors = new Color[] {Color.red, Color.green, Color.blue, Color.cyan};
@@ -30,23 +32,7 @@ public class GameManager : MonoBehaviour {
 
 		for (int row = 0; row < rows; row++) {
 			for (int column = 0; column < columns; column++) {
-
-				int colorIndex = Random.Range(0, colors.Length);
-
-				Color color = colors[colorIndex];
-
-				Vector2 position = new Vector2(startPointMatrix.position.x + (blockSize.x * column),
-				                               startPointMatrix.position.y - (blockSize.y * row));
-
-				Transform newBlock = Instantiate(blockPrefab, position, Quaternion.identity) as Transform;
-				Block properties = newBlock.GetComponent<Block>();
-
-				properties.matrixIndex = new Vector2(row, column);
-				properties.colorIndex = colorIndex;
-				properties.color = color;
-
-				newBlock.GetComponent<SpriteRenderer>().color = color;
-
+				Transform newBlock = CreateBlock(row, column);
 				matrix[row, column] = newBlock;
 			}
 		}
@@ -55,18 +41,68 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 	}
 
+	Transform CreateBlock (int row, int column) {
+
+		int colorIndex = Random.Range(0, colors.Length);
+		
+		Color color = colors[colorIndex];
+
+		Vector2 position = PositionByIndex(row, column);
+
+		Transform newBlock = Instantiate(blockPrefab, position, Quaternion.identity) as Transform;
+		Block properties = newBlock.GetComponent<Block>();
+		
+		properties.matrixIndex = new Vector2(row, column);
+		properties.colorIndex = colorIndex;
+		properties.color = color;
+		properties.scaleSize = scaleSize;
+
+		newBlock.GetComponent<SpriteRenderer>().color = color;
+
+		return newBlock;
+	}
+
 	public void BreakBlocks (Vector2 origin) {
-		int minimumCount = 3;
-		HashSet<Transform> blocksToBreak = new HashSet<Transform>();
 
-		Block blockOrigin = matrix[(int) origin.x, (int) origin.y].GetComponent<Block>();
+		canInteract = false;
 
-		BreakBlockAndAdjacents(origin, blockOrigin.colorIndex, ref blocksToBreak);
+		try {
+			int minimumCount = 3;
 
-		if (blocksToBreak.Count >= minimumCount) {
-			foreach (Transform block in blocksToBreak) {
-				Destroy(block.gameObject);
+			HashSet<Transform> blocksToBreak = new HashSet<Transform>();
+
+			Block blockOrigin = matrix[(int) origin.x, (int) origin.y].GetComponent<Block>();
+
+			BreakBlockAndAdjacents(origin, blockOrigin.colorIndex, ref blocksToBreak);
+
+			if (blocksToBreak.Count >= minimumCount) {
+				foreach (Transform blockTransform in blocksToBreak) {
+					Block block = blockTransform.GetComponent<Block>();
+
+					matrix[(int) block.matrixIndex.x, (int) block.matrixIndex.y] = null;
+
+					Destroy(blockTransform.gameObject);
+				}
+
+				List<Vector2> emptySpaces = new List<Vector2>();
+
+				RearrangeMatrix(ref emptySpaces);
+
+				if (emptySpaces.Count > 0) {
+					foreach (Vector2 index in emptySpaces) {
+						int row = (int) index.x;
+						int column = (int) index.y;
+
+						Transform newBlock = CreateBlock(row, column);
+
+						newBlock.GetComponent<Block>().ShowAnimated();
+
+						matrix[row, column] = newBlock;
+					}
+				}
 			}
+		} finally {
+			canInteract = true;
 		}
 	}
 
@@ -89,7 +125,11 @@ public class GameManager : MonoBehaviour {
 
 		Transform blockTransform = matrix[(int) index.x, (int) index.y];
 
-		if (blocksToBreak.Contains(blockTransform)) {
+		if (blockTransform == null) {
+			return;
+		}
+
+			if (blocksToBreak.Contains(blockTransform)) {
 			return;
 		}
 
@@ -105,5 +145,42 @@ public class GameManager : MonoBehaviour {
 		BreakBlockAndAdjacents(new Vector2(index.x + 1, index.y), colorIndex, ref blocksToBreak);
 		BreakBlockAndAdjacents(new Vector2(index.x, index.y - 1), colorIndex, ref blocksToBreak);
 		BreakBlockAndAdjacents(new Vector2(index.x, index.y + 1), colorIndex, ref blocksToBreak);
+	}
+
+	void RearrangeMatrix (ref List<Vector2> emptySpaces) {
+		for (int row = rows - 1; row >= 0; row--) {
+			for (int column = columns - 1; column >= 0; column--) {
+				Transform block = matrix[row, column];
+
+				if (block == null) {
+					Transform replaceBlock = null;
+
+					for (int i = row - 1; i >= 0 && replaceBlock == null; i--) {
+						Transform candidate = matrix[i, column];
+
+						if (candidate != null) {
+							replaceBlock = candidate;
+						}
+					}
+
+					if (replaceBlock != null) {
+						Block replaceProperties = replaceBlock.GetComponent<Block>();
+						matrix[(int) replaceProperties.matrixIndex.x, (int) replaceProperties.matrixIndex.y] = null;
+						replaceProperties.matrixIndex = new Vector2(row, column);
+						matrix[row, column] = replaceBlock;
+
+						replaceBlock.position = PositionByIndex(row, column);
+					
+					} else if (emptySpaces != null) {
+						emptySpaces.Add(new Vector2(row, column));
+					}
+				}
+			}
+		}
+	}
+
+	Vector2 PositionByIndex (int row, int column) {
+		return new Vector2(startPointMatrix.position.x + (blockSize.x * column),
+		                   startPointMatrix.position.y - (blockSize.y * row));
 	}
 }
